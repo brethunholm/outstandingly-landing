0900905d1a57f9dfc5c388b251982b932aeb43f0
Patrick Franz <clymbing@gmail.com>
2021-04-15 17:39:11 -0700

    Clean up Gatsby installation further Setup GlobalStyles

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,31 +1,25 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
+import * as React from 'react';
+// import { useStaticQuery, graphql } from 'gatsby';
 
-import * as React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
+import { GlobalStyle } from '../styles/GlobalStyle';
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
+  // const data = useStaticQuery(graphql`
+  //   query SiteTitleQuery {
+  //     site {
+  //       siteMetadata {
+  //         title
+  //       }
+  //     }
+  //   }
+  // `);
 
   return (
     <>
+      <GlobalStyle />
       {children}
     </>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
