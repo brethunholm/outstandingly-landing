b6d88efba42f9657f13738b6ecd603454491f0ba
Patrick Franz <clymbing@gmail.com>
2021-04-15 17:56:59 -0700

    add .gitkeep file

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,19 +1,8 @@
 import * as React from 'react';
-// import { useStaticQuery, graphql } from 'gatsby';
 
 import { GlobalStyle } from '../styles/GlobalStyle';
 
 const Layout = ({ children }) => {
-  // const data = useStaticQuery(graphql`
-  //   query SiteTitleQuery {
-  //     site {
-  //       siteMetadata {
-  //         title
-  //       }
-  //     }
-  //   }
-  // `);
-
   return (
     <>
       <GlobalStyle />
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,9 @@ import * as React from 'react';
 
 import Layout from '../components/layout';
 import Header from '../components/header';
-import Card from '../components/Card';
-
 const IndexPage = () => (
   <Layout>
     <Header />
-    <Card />
   </Layout>
 );
 
