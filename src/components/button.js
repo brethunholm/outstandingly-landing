7a04923d2ce32fff66a946cc98cc2b915e663243
Patrick Franz <clymbing@gmail.com>
2021-04-25 14:01:59 -0700

    refactor: button: add text prop to button. style: modify style

--- src/components/button.js
+++ src/components/button.js
@@ -2,27 +2,23 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledButton = styled.span`
-  .bnt {
-    background: #FE6192;
-    color: white;
-    border: none;
-    border-radius: 1px;
-    padding: 0.5rem;
-    font-size: .5rem;
-    height: 2rem;
-    text-align: center;
-    font-weight: bold;
-  }
-  .bnt:hover {
-    background: #F2E088;
-    color: #FE6192;
+  background: #fe6192;
+  color: white;
+  border: none;
+  border-radius: 1px;
+  padding: 0.5rem;
+  font-size: 0.5rem;
+  height: 2rem;
+  text-align: center;
+  font-weight: bold;
+
+  &:hover {
+    background: #f2e088;
+    color: #fe6192;
     font-weight: bold;
   }
 `;
 
-export default function Button() {
-  return <StyledButton>
-    <button class="bnt">BUTTON TEXT</button>
-    
-    </StyledButton>;
+export default function Button(props) {
+  return <StyledButton>{props.text}</StyledButton>;
 }
