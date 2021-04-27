d7a79490cf48e5f62a52a1980902e3162a7b3b6c
Patrick Franz <clymbing@gmail.com>
2021-04-27 08:14:28 -0700

    Change Typography config to get rid of theme. It was messing with link tags

--- src/styles/Typography.js
+++ src/styles/Typography.js
@@ -1,6 +1,12 @@
 import Typography from 'typography';
 import lincolnTheme from 'typography-theme-lincoln';
 
-const TypographyBase = new Typography(lincolnTheme);
+// const TypographyBase = new Typography(lincolnTheme);
+const TypographyBase = new Typography({
+  baseFontSize: '18px',
+  baseLineHeight: 1.666,
+  headerFontFamily: ['Segoe UI', 'Helvetica', 'Arial', 'sans-serif'],
+  bodyFontFamily: ['Georgia', 'serif'],
+});
 
 export default TypographyBase;
