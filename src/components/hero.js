dab1000703c10f04905272b43fbe410a26afa7c4
Bre Thunholm <bthunholm@gmail.com>
2021-04-26 14:49:52 -1000

    added .eslintrc configured eslint and prettier settings deleted .prettierrc wes bos told me to

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,38 +1,34 @@
-import React from 'react'
+import React from 'react';
 import styled from 'styled-components';
-import SectionContainer from './section-container'
-import Cover from '../videos/blue-liquid.mp4'
-import VideoBg from "reactjs-videobg";
+import VideoBg from 'reactjs-videobg';
+import SectionContainer from './section-container';
+import Cover from '../videos/blue-liquid.mp4';
 
 const StyledHero = styled.div`
-height: 30%;
-width: 100vw;
-display: grid;
-justify-content: center;
-align-items: center;
-background: #4ED5EF;
-.hero {
- height:100%;
-  position: absolute;
-  top: 0;
-  z-index: -1;
-}
+  height: 30%;
+  width: 100vw;
+  display: grid;
+  justify-content: center;
+  align-items: center;
+  background: #4ed5ef;
+  .hero {
+    height: 100%;
+    position: absolute;
+    top: 0;
+    z-index: -1;
+  }
 `;
 
 export default function Hero() {
-   return (
-      <SectionContainer>
-          <StyledHero>
-              <h1>Outstandingly</h1>
-         <VideoBg videoClass='hero' loop>
-        <VideoBg.Source  src={Cover} type="video/mp4" />
-        </VideoBg>;
-
-          </StyledHero>
-
-
-
-
-      </SectionContainer>
-   )
+  return (
+    <SectionContainer>
+      <StyledHero>
+        <h1>Outstandingly</h1>
+        <VideoBg videoClass="hero" loop>
+          <VideoBg.Source src={Cover} type="video/mp4" />
+        </VideoBg>
+        ;
+      </StyledHero>
+    </SectionContainer>
+  );
 }
