83f4a40749646c538a5022f2fb42e3eec9f9ec80
Bre Thunholm <bthunholm@gmail.com>
2021-04-27 13:19:21 -1000

    updated hero section css

--- src/components/hero.js
+++ src/components/hero.js
@@ -4,34 +4,127 @@ import VideoBg from 'reactjs-videobg';
 import Cover from '../videos/blue-liquid.mp4';
 
 const StyledHero = styled.div`
-  height: 40%;
-  width: 100vw;
   display: grid;
   justify-content: center;
   align-items: center;
   background: transparent;
+  color: var(--white);
+  padding: 4rem;
+  text-shadow: 2px 4px 3px rgba(0, 0, 0, 0.3);
+
   h1 {
-    color: var(--white);
-    font-size: 4rem;
-    padding: 2rem;
+    font-size: 5rem;
+    padding: 0.3rem;
     font-weight: bold;
-    line-height: 0.9em;
   }
 
-  .hero {
-    /* height: 100vh;
-    width: 100vw; */
+  .hero-content {
+    display: grid;
+    justify-content: center;
+    grid-template-columns: 1fr;
+  }
+
+  .top-span {
+    justify-self: center;
+    font-size: 1.3rem;
+  }
+
+  hr {
+    height: 1px;
+    /* margin: 50px 0; */
+    background: -webkit-gradient(
+      linear,
+      0 0,
+      100% 0,
+      from(rgba(0, 0, 0, 0)),
+      color-stop(0.5, #333333),
+      to(rgba(0, 0, 0, 0))
+    );
+    background: -webkit-linear-gradient(
+      left,
+      rgba(0, 0, 0, 0),
+      #333333,
+      rgba(0, 0, 0, 0)
+    );
+    background: -moz-linear-gradient(
+      left,
+      rgba(0, 0, 0, 0),
+      #333333,
+      rgba(0, 0, 0, 0)
+    );
+    background: -o-linear-gradient(
+      left,
+      rgba(0, 0, 0, 0),
+      #333333,
+      rgba(0, 0, 0, 0)
+    );
+    background: linear-gradient(
+      left,
+      rgba(0, 0, 0, 0),
+      #333333,
+      rgba(0, 0, 0, 0)
+    );
+    border: 0;
+  }
+  hr:after {
+    display: block;
+    content: '';
+    height: 30px;
+    background-image: -webkit-gradient(
+      radial,
+      50% 0%,
+      0,
+      50% 0%,
+      116,
+      color-stop(0%, #cccccc),
+      color-stop(100%, rgba(255, 255, 255, 0))
+    );
+    background-image: -webkit-radial-gradient(
+      center top,
+      farthest-side,
+      #cccccc 0%,
+      rgba(255, 255, 255, 0) 100%
+    );
+    background-image: -moz-radial-gradient(
+      center top,
+      farthest-side,
+      #cccccc 0%,
+      rgba(255, 255, 255, 0) 100%
+    );
+    background-image: -o-radial-gradient(
+      center top,
+      farthest-side,
+      #cccccc 0%,
+      rgba(255, 255, 255, 0) 100%
+    );
+    background-image: radial-gradient(
+      farthest-side at center top,
+      #cccccc 0%,
+      rgba(255, 255, 255, 0) 100%
+    );
+  }
+  .hero-bg {
     position: fixed;
-    /* top: 0;
-    z-index: -1; */
+  }
+
+  @media (max-width: 900px) {
+    .hero-bg {
+      display: none;
+    }
+    background: #259fcb;
   }
 `;
 
 export default function Hero() {
   return (
     <StyledHero>
-      <h1>OUTSTANDINGLY</h1>
-      <VideoBg videoClass="hero" loop>
+      <div className="hero-content">
+        <span className="top-span">SOMETHING SOMETHING</span>
+        <h1>OUTSTANDINGLY</h1>
+        <hr />
+      </div>
+
+      <VideoBg videoClass="hero-bg" loop>
         <VideoBg.Source src={Cover} type="video/mp4" />
       </VideoBg>
     </StyledHero>
