e04e822042cc4c64780bbca960fcb9725f7dc5fc
Patrick Franz <clymbing@gmail.com>
2021-06-04 15:26:34 -0700

    benefits section update style: change up the style to the benefits cards by adding images

--- src/components/BenefitsCard.js
+++ src/components/BenefitsCard.js
@@ -1,48 +1,74 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styled from 'styled-components';
+import { BgImage } from 'gbimage-bridge';
+
+const StyledBgImage = styled(BgImage)`
+  width: 100%;
+  height: 100% !important;
+  border-radius: 15px;
+`;
 
 const StyledCard = styled.div`
-  height: 375px;
-  width: 300px;
-  overflow: hidden;
+  display: flex;
+  flex-wrap: wrap;
   border-radius: 5px;
-  background-color: var(--white);
-  display: grid;
-  grid-template-rows: 1fr 0.75fr 2.25fr;
+  background-color: white;
+  border: 3px solid var(--highlight-pink);
+  min-height: 350px;
   justify-items: center;
-  margin: 15px;
   padding: 5px;
+  gap: 30px;
+  max-width: 600px;
+  min-width: 300px;
+  padding: 10px;
   transition: 250ms ease-in-out;
+
   &:hover {
-    /* transform: scale(1.01); */
     box-shadow: var(--card-shadow);
   }
+  .image {
+    flex: 1;
+    max-width: 300px;
+    min-width: 300px;
+    min-height: 150px;
+  }
 
-  p {
-    color: var(--grey-darker);
-    line-height: 1.2rem;
-    font-size: 20px;
-    text-align: left;
-    margin: 0;
-  }
-  h3 {
-    place-self: center;
-    margin: 0;
-    padding: 0;
-  }
-  .icon {
-    place-self: center;
-    font-size: 1.8rem;
-    color: var(--accent-dark);
+  .content-wrap {
+    flex: 1;
+
+    p {
+      color: var(--grey-darker);
+      line-height: 1.2rem;
+      font-size: 20px;
+      text-align: left;
+      margin: 0;
+    }
+    h3 {
+      place-self: center;
+      margin: 0;
+      padding: 0;
+    }
+    .icon {
+      place-self: center;
+      font-size: 1.8rem;
+      color: var(--accent-dark);
+      margin-bottom: 15px;
+    }
   }
 `;
-export default function BenefitsCard({ title, icon, content }) {
+export default function BenefitsCard({ title, icon, content, image }) {
+  console.log(image);
   return (
     <StyledCard>
-      <FontAwesomeIcon className='icon' icon={icon} />
-      <h3>{title}</h3>
-      <p>{content}</p>
+      <div className='image'>
+        <StyledBgImage image={image} alt='' />
+      </div>
+      <div className='content-wrap'>
+        <FontAwesomeIcon className='icon' icon={icon} />
+        <h3>{title}</h3>
+        <p>{content}</p>
+      </div>
     </StyledCard>
   );
 }
---
+++ src/components/SideBySide.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import styled from 'styled-components';
+import Button from './button';
+
+const StyledCard = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 2px 1fr;
+  color: var(--black);
+  padding: 75px 40px;
+  gap: 1rem;
+  grid-template-areas: 'title vl text_content';
+
+  &.flipped {
+    grid-template-areas: 'text_content vl title' !important;
+  }
+
+  .title {
+    grid-area: title;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .highlight {
+    color: var(--highlight-pink);
+  }
+
+  .vl {
+    border: none;
+    grid-area: vl;
+    border-left: 5px solid var(--black);
+  }
+
+  .content {
+    position: relative;
+    grid-area: text_content;
+    padding: 20px;
+    text-align: justify;
+    line-height: 30px;
+
+    .seperator {
+      width: 150px;
+      border-bottom: 5px solid var(--highlight-pink);
+      margin: auto;
+      bottom: 0;
+    }
+  }
+
+  @media (max-width: 900px) {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-areas:
+      'title'
+      'vl'
+      'content';
+
+    .vl {
+      border: none;
+      border-bottom: 5px solid var(--black);
+    }
+
+    .content {
+      padding: 0;
+      margin: 0;
+    }
+  }
+
+  @media (max-width: 450px) {
+    .content {
+      padding: 0;
+      margin: 0;
+    }
+    h1 {
+      font-size: 1rem;
+    }
+    p {
+      font-size: 0.75rem;
+    }
+  }
+`;
+
+export default function SideBySide({ headline, btnText, sideText, flipped }) {
+  console.log(flipped);
+  return (
+    <StyledCard className={flipped ? 'flipped' : ''}>
+      <div className='title'>
+        <h1 className='heading'>{headline}</h1>
+        <Button text={btnText} />
+      </div>
+      <div className='vl' />
+      <div className='content'>
+        {sideText}
+        <div className='seperator' />
+      </div>
+    </StyledCard>
+  );
+}
