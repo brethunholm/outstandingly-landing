4d0b161996016d78dbadbcc7412de259e4fdca61
Bre Thunholm <bthunholm@gmail.com>
2021-06-29 09:12:36 -0500

    fixed typeo in whatIsOutstandingly section

--- src/components/whatIsOutstandingly.js
+++ src/components/whatIsOutstandingly.js
@@ -6,7 +6,7 @@ export default function WhatIsOutstandingly() {
   const headline = (
     <span>
       Break out of the endless tutorial cycle and learn{' '}
-      <span className='highlight'>web development</span> on the job
+      <span className="highlight">web development</span> on the job
     </span>
   );
 
@@ -14,7 +14,7 @@ export default function WhatIsOutstandingly() {
     <p>
       We offer newer developers the next step to gain the experience required to
       get their dream job. Our goal is to provide a productive, accessible,
-      collaborative wwork environment for new developers who are struggling to
+      collaborative work environment for new developers who are struggling to
       get their foot in the door. Collaborative project-based Creator Teams work
       together to create high-quality portfolio projects, gain real-world
       experience, and work in a development team environment while building a
@@ -24,9 +24,9 @@ export default function WhatIsOutstandingly() {
 
   return (
     <SideBySide
-      id='whatIs'
+      id="whatIs"
       headline={headline}
-      btnText='SHOW ME HOW'
+      btnText="SHOW ME HOW"
       sideText={sideText}
     />
   );
